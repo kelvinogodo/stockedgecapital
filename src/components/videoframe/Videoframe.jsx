4408aafebaeab2f8587e24953ff5437da77d78b3
trilvid <trilztech34@gmail.com>
2025-03-18 23:43:38 +0100

    first commit

--- src/components/videoframe/Videoframe.jsx
+++ src/components/videoframe/Videoframe.jsx
@@ -7,7 +7,7 @@ const Videoframe = () => {
         <div className="videoframe-text-container">
           <h1>where the <span className="highlight">forex </span>market is going</h1>
         </div>
-              <video src="/chart-big.hvc1.6af4110d38611a03c3a4.mp4" className='video' autoPlay='true'></video>
+              <video src="/chart-big.hvc1.6af4110d38611a03c3a4.mp4" className='video' autoPlay='true' ></video>
           </div>
     </section>
   )
