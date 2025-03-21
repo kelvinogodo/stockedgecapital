5c79805ed359e972df4d7c0aecbbf65511eeab52
trilvid <trilztech34@gmail.com>
2025-03-21 12:48:37 +0100

    wrote media queries

--- src/components/ForexAnalysisSection/ForexAnalysisSection.jsx
+++ src/components/ForexAnalysisSection/ForexAnalysisSection.jsx
@@ -10,14 +10,14 @@ const ForexAnalysisSection = () => {
             </div>
           <div className="trading-view-forex-wrapper">
               <div className="trading-view-card">
-                  <div className="trading-view-card-text-container">
+                  <div className="trading-view-card-text-container" data-aos="fade-up">
                       <h1>Technical Analysis</h1>
                       <p>See what the technical analysis says about a given symbol with our display ratings, made for easy viewing.</p>
                   </div>
                   <TradingViewFinancials />
               </div>
               <div className="trading-view-card">
-                  <div className="trading-view-card-text-container">
+                  <div className="trading-view-card-text-container" data-aos="fade-up">
                       <h1>Fundamental Data</h1>
                       <p>Kick the tires on the fundamentals with this deep dive into how a company is doing beyond simply its stock price.</p>
                   </div>
--- src/components/Teslawidget/TeslaWidgetContainer.jsx
+++ src/components/Teslawidget/TeslaWidgetContainer.jsx
@@ -11,7 +11,7 @@ const TeslaWidgetContainer = () => {
               <p>Grab an overview of global markets including price changes, open, high, low, and close values for selected instruments.</p>
         </div>
           <div className="tesla-widget-wrapper">
-              <div className="tesla-widget-text-container">
+              <div className="tesla-widget-text-container" data-aos="fade-up">
                   <h1>Symbol Overview</h1>
                   <p>Embed the latest quotes, plus a simple chart of a single symbol of your choice. A great option for any web or mobile-geared pages.</p>
                   <div className="tesla-widget-btn-container">
